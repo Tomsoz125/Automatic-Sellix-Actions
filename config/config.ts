85c35e261c1da8a1bbe0eb144d8a9fd3c3119c24
Tomsoz125 <razertomsoz@gmail.com>
2024-11-24 03:48:05 +0000

    Updated config

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,8 @@ import { config as dotenv } from "dotenv";
 dotenv();
 
 export const config = {
-	PORT: process.env.PORT || 3000,
-	DATABASE_URL: process.env.DATABASE_URL || ""
+	PORT: process.env.PORT || 25596,
+	DATABASE_URL: process.env.DATABASE_URL || "",
+	SELLIX_WEBHOOK_SECRET: process.env.SELLIX_WEBHOOK_SECRET || "",
+	BOT_TOKEN: process.env.BOT_TOKEN || ""
 };
