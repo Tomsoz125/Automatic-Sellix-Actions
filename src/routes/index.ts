90c830f4b7101834e7ca18caf84991a846601394
Tomsoz125 <razertomsoz@gmail.com>
2024-11-24 04:19:23 +0000

    Bug fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,11 @@
 import { Router } from "express";
-import fs from "fs";
-import path from "path";
 import userRoutes from "./userRoutes";
+import webhookRoutes from "./webhooks";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 
-const webhooksPath = path.join(__dirname, "webhooks");
-fs.readdirSync(webhooksPath).forEach((file) => {
-	if (file.endsWith(".ts") || file.endsWith(".js")) {
-		const routeName = file.replace(/\.[tj]s$/, ""); // Remove extension
-		const route = require(path.join(webhooksPath, file)).default;
-		router.use(`/webhooks/${routeName}`, route);
-	}
-});
+router.use("/webhooks", webhookRoutes);
 
 export default router;
