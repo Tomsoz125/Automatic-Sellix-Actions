6e0badd9a7c469c848e44d76f872fa657955c329
Tomsoz125 <razertomsoz@gmail.com>
2024-11-29 14:26:38 +0000

    Database fix

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,6 +7,7 @@ config(); // Load environment variables
 const pool = mysql.createPool({
 	host: process.env.MYSQL_HOST || "",
 	user: process.env.MYSQL_USER || "",
+	port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 3306,
 	password: process.env.MYSQL_PASSWORD || "",
 	database: process.env.MYSQL_DATABASE || "",
 	waitForConnections: true,
